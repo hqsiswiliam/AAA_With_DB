5402daad0c23dc9bb8d9668b9b55e05a15e0bfc3
hqsiswiliam <hqsiswiliam@gmail.com>
2013-10-22 23:54:47 +0100

    AAA Sites with Database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AAAWebsite::Application.config.secret_token = '4c7c52bc753a2c4c65279196b1c6890dfb98d5c7877de893c6ff594c02d45a4a12ffca90e9adb418800948863b245dc8954a7e4b78360956e1119721b7e7fbab'
